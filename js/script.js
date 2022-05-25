dded988e7cbea3c6e11e2c4b4806aa9907307d8c
Casey MacLeod <caseymacleod22@gmail.com>
2022-05-25 11:20:29 -0400

    wip: logs all usernames

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ let playerData;
 
 
 function getData() {
-    $.ajax('https://api.sleeper.app/v1/league/784456593403224064/rosters')
+    $.ajax('https://api.sleeper.app/v1/league/784456593403224064/users')
     .then(function(data) {
         playerData = data
-        console.log(playerData)
+        console.log(playerData[0].display_name, playerData[1].display_name, playerData[2].display_name, playerData[3].display_name, playerData[4].display_name, playerData[5].display_name, playerData[6].display_name, playerData[7].display_name, playerData[8].display_name,playerData[9].display_name)
     })
+}
+
+function mapUserData() {
+
 }
 
 getData()
