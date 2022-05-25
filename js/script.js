405f9bd55da0487359db1c05e0a5f3ee5a5bfe2b
Casey MacLeod <caseymacleod22@gmail.com>
2022-05-25 11:41:56 -0400

    fetch usernames

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
-const BASE_URL = 'https://api.sleeper.app/v1/'
-let playerData;
+// const BASE_URL = 'https://api.sleeper.app/v1/'
+// let playerData;
 
 
 function getData() {
     $.ajax('https://api.sleeper.app/v1/league/784456593403224064/users')
     .then(function(data) {
-        playerData = data
-        console.log(playerData[0].display_name, playerData[1].display_name, playerData[2].display_name, playerData[3].display_name, playerData[4].display_name, playerData[5].display_name, playerData[6].display_name, playerData[7].display_name, playerData[8].display_name,playerData[9].display_name)
+        playerData = data  
+        console.log(playerData[0].display_name)
     })
 }
 
 function mapUserData() {
-
+    
 }
 
 getData()
 // 784456593403224064
+
+
+// async function fetchData() {
+
+//     const response = await fetch('https://api.sleeper.app/v1/league/784456593403224064/users');
+//     const data = await response.json();
+
+//     data.forEach(obj => {
+//         Object.entries(obj).forEach(([key, value]) => {
+//             console.log(`${key} ${value}.`);
+//         });
+//         console.log('-------------------');
+//     });
+// }
+
+// function fetchUserName() {
+//     fetchData()
+// }
+
+// fetchUserName()
