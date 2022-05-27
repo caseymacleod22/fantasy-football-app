02fa083dba3579e426ac8568f4cd42877432cfe2
Casey MacLeod <caseymacleod22@gmail.com>
2022-05-26 22:22:00 -0400

    wip: display team name

--- js/script.js
+++ js/script.js
@@ -17,4 +17,21 @@ $(document).ready(function() {
             }
     })
 })
+
+$(document).ready(function() {
+    $.ajax({
+            url: 'https://api.sleeper.app/v1/league/784456593403224064/users',
+            type: 'GET',
+            success: function(result) {
+                playerData = result.map(result => result.metadata.team_name);
+                console.log(playerData);
+                $(".team-name").append(JSON.stringify(playerData));
+                // playerData = result
+                // console.log(playerData)
+            },
+            error: function(err) {
+                console.log(err)
+            }
+    })
+})
     
