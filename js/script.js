289a993b4576aa7360895d722b25aa3c0c0e5a70
Casey MacLeod <caseymacleod22@gmail.com>
2022-06-04 01:20:05 -0400

    wip: comments out avatar map function

--- js/script.js
+++ js/script.js
@@ -30,18 +30,18 @@ $(document).ready(function() {
     })
 });
 
-$(document).ready(function() {
-    $.ajax({
-            url: 'https://api.sleeper.app/v1/league/784456593403224064/users',
-            type: 'GET',
-            success: function(result) {
-                playerData = result.map(result => result.metadata.avatar);
-                playerData.forEach((avatar, item) => {
-                    $(`#avatar${item}`).append(avatar);
-                });
-            },
-            error: function(err) {
-                console.log(err)
-            }
-    })
-});
+// $(document).ready(function() {
+//     $.ajax({
+//             url: 'https://api.sleeper.app/v1/league/784456593403224064/users',
+//             type: 'GET',
+//             success: function(result) {
+//                 playerData = result.map(result => result.metadata.avatar);
+//                 playerData.forEach((avatar, item) => {
+//                     $(`#avatar${item}`).append(avatar);
+//                 });
+//             },
+//             error: function(err) {
+//                 console.log(err)
+//             }
+//     })
+// });
