87a320b1119475cffb230c9aea3ec40f84d5db37
Casey MacLeod <caseymacleod22@gmail.com>
2022-06-06 22:47:40 -0400

    wip: accordion for league info dump

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+// League info accordion function
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 $(document).ready(function() {
     $.ajax({
             url: 'https://api.sleeper.app/v1/league/784456593403224064',
