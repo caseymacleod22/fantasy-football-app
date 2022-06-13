855bbf16227480c0068b80d078338ab84f413976
Casey MacLeod <caseymacleod22@gmail.com>
2022-06-12 22:29:22 -0400

    comments out console logs

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
                   });
             },
             error: function(err) {
-                console.log(err)
+                // console.log(err)
             }
     })
 });
